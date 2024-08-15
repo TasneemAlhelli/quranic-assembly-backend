94f2d1dd24bedf4c1a49cbce9e65259d39829d89
Tasneem <tasneem.jameel9@gmail.com>
2024-08-15 19:37:17 +0300

    Characters

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Api\AchievementController;
 use App\Http\Controllers\Api\SoiareeController;
 use App\Http\Controllers\Api\CompetitionController;
 use App\Http\Controllers\Api\PoetryController;
+use App\Http\Controllers\Api\CharacterController;
 
 Route::resource('/centers', CenterController::class);
 
@@ -17,3 +18,5 @@ Route::resource('/soiaress', SoiareeController::class);
 Route::resource('/competitions', CompetitionController::class);
 
 Route::resource('/poetries', PoetryController::class);
+
+Route::resource('/characters', CharacterController::class);
